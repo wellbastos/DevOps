3a0c9545eaf9fd561bf5e1d5f31333231985d933
Rafael Miranda <rafaelmiranda178@gmail.com>
2019-12-16 19:18:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # default route
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'Hello World Teste Segunda-Feira!'
 
 # function for responses
 def results():
